b1f900c9b09ca27103c5df1a7403fefb2882fb76
Disruptor <davidsantiagojimenez16@gmail.com>
2024-04-30 12:09:27 -0500

    Se realiza las APIS que conectan a mongo con mongoose, GET GET Y PUT, funcionando correctamente

--- UsersInformation/src/index.js
+++ UsersInformation/src/index.js
@@ -2,7 +2,6 @@ import express from 'express';
 import {PORT} from './config.js';
 import userRoutes from './routes/info.routes.js';
 import mongoose from 'mongoose';
-import bodyParser from 'body-parser';
 const app = express();
 
 
@@ -10,7 +9,7 @@ app.use(userRoutes);
 
 
 
-mongoose.connect('mongodb+srv://davidsantiagojimenez16:kLxzdcyp34opAz4I@infodb.qcvtdg7.mongodb.net/Info-API?retryWrites=true&w=majority&appName=InfoDB')
+mongoose.connect('LINK')
   .then(() => {
     console.log("Connected to MongoDB!");
     app.listen(PORT, () => console.log('Server running on port ' + PORT));
