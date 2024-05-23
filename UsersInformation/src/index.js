b1abf102ad3dc703e760da121c1920c114f5141f
Disruptor <davidsantiagojimenez16@gmail.com>
2024-05-22 22:59:05 -0500

    Hola

--- UsersInformation/src/index.js
+++ UsersInformation/src/index.js
@@ -2,15 +2,16 @@ import express from 'express';
 import {PORT} from './config.js';
 import userRoutes from './routes/info.routes.js';
 import mongoose from 'mongoose';
-import bodyParser from 'body-parser';
 const app = express();
 
-
+app.use(express.json());
+//app.use(express.urlencoded({extended}))
 app.use(userRoutes);
 
 
 
-mongoose.connect('mongodb+srv://davidsantiagojimenez16:kLxzdcyp34opAz4I@infodb.qcvtdg7.mongodb.net/Info-API?retryWrites=true&w=majority&appName=InfoDB')
+
+mongoose.connect('mongodb+srv://davidsantiagojimenez16:kLxzdcyp34opAz4I@infodb.qcvtdg7.mongodb.net/?retryWrites=true&w=majority&appName=InfoDB')
   .then(() => {
     console.log("Connected to MongoDB!");
     app.listen(PORT, () => console.log('Server running on port ' + PORT));
--- UsersInformation/src/routes/info.routes.js
+++ UsersInformation/src/routes/info.routes.js
@@ -1,39 +1,19 @@
 import { Router } from "express";
 import infoUser from "../models/infoUsers.model.js";
+import { postUser,getAllUsers, deleteUser, updateUser} from "../controllers/postInfo.controller.js";
 import bodyParser from "body-parser";
 
 const router = Router();
 var jsonParser = bodyParser.json()
 
-router.post('/info', jsonParser, async(req,res) => {
-    try{
-        const user = await infoUser.create(req.body);
-        res.status(201).json(user);
-    } catch (error) {
-        console.log(error);
-        res.status(500).send(error);
-    }
-});
 
+router.post('/info',postUser);
 
-router.get('/info', async(req,res) => {
-    try {
-        const Users = await infoUser.find({});
-        res.status(200).json(Users);
-    } catch (error) {
-        res.status(500).send(error);
-    }
-});
+router.get('/info',getAllUsers);
 
-router.get('/info/:id', async(req,res) => {
-    try {
-        const { id } = req.params;
-        const user = await infoUser.findById(id);
-        res.status(201).json(user);
-    } catch (error) {
-        res.status(500).send(error);
-    }
-});
+router.delete('/info/:id',deleteUser);
+
+router.put('/info/:id',updateUser);
 
 
 export default router;
